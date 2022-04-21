9a972228012a9367fde404e7078063b97b570c12
bansari278 <64257982+bansari278@users.noreply.github.com>
2022-04-21 12:35:43 +0530

    Add files via upload

---
+++ PIZZA CART ANGULAR/frontend/src/app/products/products.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+import { User } from '@app/_models';
+import { AccountService } from '@app/_services';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  user: User;
+  public productList : any ;
+  public filterCategory : any
+  searchKey:string ="";
+  constructor(private accountService: AccountService,private api : ApiService, private cartService : CartService) {
+  this.user = this.accountService.userValue;
+
+   }
+
+  ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
+      this.productList.forEach((a:any) => {
+        // if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+        //   a.category ="fashion"
+        // }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList)
+    });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
+  }
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
+  }
+  filter(category:string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
+  }
+
+}
